4cfe5fc6fe20890acad4dbb660746ccf8f41d041
Kovács Máté <kismate90@gmail.com>
2018-11-28 15:57:11 +0100

    Created application server.

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// Application for SpringTab test
+var express = require('express');
+var app = express();
+
+app.listen(3000, function() {
+    console.log('Started application server at http://localhost:3000');
+});
