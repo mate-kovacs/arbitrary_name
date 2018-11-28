4af4d8b2a97d908fb63a44be4cd92e637ead3b9d
Kovács Máté <kismate90@gmail.com>
2018-11-28 15:59:57 +0100

    Created get request handler for route '/helloworld'.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.get('/helloworld', function (req, res) {
+    let content = {'message': 'ezigen'};
+    res.contentType('application/json');
+    res.send(content);
+});
+
 app.listen(3000, function() {
     console.log('Started application server at http://localhost:3000');
 });
